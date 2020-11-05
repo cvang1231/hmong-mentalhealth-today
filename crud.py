4ebf0069ffc419ee2b45d493fd2832381ae5ceda
cvang1231 <vangchandini@gmail.com>
2020-11-05 21:23:01 +0000

    Created favorite functions.

---
+++ crud.py
@@ -0,0 +1,107 @@
+"""CRUD operations."""
+
+from model import db, User, Therapist, Favorite, connect_to_db
+
+
+# user functions
+
+
+def create_user(email, password, zipcode):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password, zipcode=zipcode)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+def get_users():
+    """Return all users in database."""
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """Return a user by their user_id."""
+
+    return User.query.filter(User.user_id == user_id).first()
+
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+
+# therapist functions
+
+
+def add_therapist(name, email, website, lat_loc, lon_loc, specialty, img)
+    """Create and return a new therapist."""
+
+    therapist = Therapist(name=name 
+                        email=email
+                        website=website
+                        lat_loc=lat_loc
+                        long_loc=long_loc
+                        specialty=specialty
+                        img=img)
+                        )
+
+    db.session.add(therapist)
+    db.session.commit()
+
+    return therapist
+
+
+def get_therapists():
+    """Return all therapists in database."""
+
+    return Therapist.query.all()
+
+
+def get_therapist_by_id(therapist_id):
+    """Return a therapist by their therapist_id."""
+
+    return Therapist.query.filter(Therapist.therapist_id == therapist.id).first()
+
+
+def get_therapist_by_email(email):
+    """Return a therapist by email."""
+
+    return Therapist.query.filter(Therapist.email == email).first()
+
+
+# favorite functions
+
+
+def create_fav(user_id, therapist_id):
+    """Create and return a new favorite."""
+
+    favorite = Favorite(user_id=user_id, therapist_id=therapist_id)
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return favorite
+
+
+def get_fav_therapists(user_id):
+    """Return a user's favorite therapists."""
+
+    list_of_therapists = []
+    user_favs = Favorite.query.filter(Favorite.user_id = user_id).all()
+
+    for fav in user_favs:
+
+        therapist = Therapist.query.filter(Therapist.therapist_id == therapist_id).all()
+
+        list_of_therapists.append(therapist)
+
+    return list_of_therapists
+    
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
