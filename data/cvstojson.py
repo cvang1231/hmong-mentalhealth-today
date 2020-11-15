7a7b6c4a9e0d338bd473ee87ae278d1c49ced32e
cvang1231 <vangchandini@gmail.com>
2020-11-15 21:29:12 +0000

    From DictReader to cvstojson.py. Added lat and long columns.

---
+++ data/cvstojson.py
@@ -0,0 +1,15 @@
+import json
+import csv
+
+csvFilePath = 'therapists.csv'
+jsonFilePath = 'therapists.json'
+
+# Read the CSV and add data to dictionary
+data = {}
+with open(csvFilePath) as f:
+    reader = csv.DictReader(f)
+    rows = list(reader)
+
+# Write data to a JSON file
+with open(jsonFilePath, 'w') as f:
+    json.dump(rows, f, indent = 4)
