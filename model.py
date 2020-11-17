977fcabfd18ae4f4aaed09ef469bdcc318b0a5f7
cvang1231 <vangchandini@gmail.com>
2020-11-17 20:51:09 +0000

    Got rid of fav_id from User table and added db.relationship to Favorite table.

--- model.py
+++ model.py
@@ -26,9 +26,6 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(20), nullable=False)
     zipcode = db.Column(db.Integer)
-    #fav_id = db.Column(db.Integer, db.ForeignKey('favorites.fav_id'))
-
-    # favorite = a list of Favorite objects
 
     def __repr__(self):
 
@@ -50,66 +47,32 @@ class Therapist(db.Model):
     lat = db.Column(db.Numeric(precision=9, scale=2), nullable=False)
     long = db.Column(db.Numeric(precision=9, scale=2), nullable=False)
     img = db.Column(db.String)
+    county = db.Column(db.String)
 
     def __repr__(self):
 
         return f'<Therapist thrpst_id={self.thrpst_id} name={self.name} email={self.email}>'
 
 
-#class Favorite(db.Model):
- #   """A favorite therapist object."""
+class Favorite(db.Model):
+    """A favorite."""
 
-  #  __tablename__ = 'favorites'
+    __tablename__ = 'favorites'
 
     # primary key for favorites table
-   # fav_id = db.Column(db.Integer, autoincrement=True primary_key=True)
-    # foreign key from therapists table
-    #thrpst_id = db.Column(db.Integer, db.ForeignKey('therapists.thrpst_id'))
+    fav_id = db.Column(db.Integer, primary_key=True)
     # foreign key from users table
-    #user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    # foreign key from therapists table
+    thrpst_id = db.Column(db.Integer, db.ForeignKey('therapists.thrpst_id'))
 
-    #def __repr__(self) -> str:
-     #   return f'<Favorite fav_id={self.fav_id} thrpst_id={self.thrpst_id} user_id={self.user_id}>'
+    user = db.relationship('User', backref='favorites')
+    therapist = db.relationship('Therapist', backref='favorites')
 
+    def __repr__(self):
 
-#def example_data():
+        return f'<Favorite fav_id={self.fav_id} therapist_id={self.thrpst_id} user_id={self.user_id}>'
 
- #   User.query.delete()
-  #  Therapist.query.delete()
-   # Favorite.query.delete()
-
-    #skip = User(
-     #           user_id=20,
-      #          email='skipskippy@test.com',
-       #         password='skip1234',)
-    #molly = User(
-     #           user_id=70,
-      #          email='mollym@test.com',
-       #         password='molly1234')
-    
-    #mai = Therapist(
-     #           thrpst_id=95,
-      #          name='Mai Xiong Yang',
-       #         clinic='Hennepin County',
-        #        website='blahblahblah',
-         #       email='maixy@test.com',
-          #      specialty='my bio',)
-    #tou = Therapist(
-     #           therpst_id=110,
-      #          name='Tou Vang',
-       #         clinic='Fairview Hospital',
-        #        website='blehblehbleh',
-         #       email='touv@test.com',
-          #      specialty='my bio',)
-
-    #fav_1 = Favorite(
-     #           fav_id=1,
-      #          user_id=20,
-       #         thrpst_id=95)
-    #fav_2 = Favorite(
-     #           fav_id=2,
-      #          user_id=70,
-           #     thrpst_id=110)
 
 
 
