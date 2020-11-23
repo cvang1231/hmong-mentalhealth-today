b19716571418c4ab10be9c4d4ff9749800f551a3
cvang1231 <vangchandini@gmail.com>
2020-11-23 17:42:18 +0000

    Added filter therapists by county feature using jQuery.

---
+++ static/main.js
@@ -0,0 +1,18 @@
+'use Strict';
+
+
+
+
+// filter therapists by county
+$(function(){
+    $('form').on('submit',function(){
+        event.preventDefault();
+        var value=$("input[name=county]:checked").val();
+        switch(value){
+            case "dakota": location.href='/therapists/dakota-county'; break;
+            case "hennepin": location.href='/therapists/hennepin-county'; break;
+            case "ramsey": location.href='/therapists/ramsey-county'; break;
+            case "washington": location.href='/therapists/washington-county'; break;
+        }
+    });
+});
